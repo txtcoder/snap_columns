4b1958c0c8f4a3102b499de1f28008d975d4ee0f
snada <stefano.nada@gmail.com>
2017-12-25 00:20:18 +0100

    Also stdin

--- print.rb
+++ print.rb
@@ -2,13 +2,12 @@
 
 require_relative 'snap_columns/snap_columns'
 
-words = "a bbs c sd e f g"
-words = words.split(' ')
+words = ARGF.read.chomp.split(' ')
 
 matrix =  SnapColumns::get(words)
 
 matrix.first.size.times do |row|
   puts "#{matrix[0][row]}#{' ' * (matrix[0].max_size - matrix[0][row].to_s.size)} " +
-    "#{matrix[1][row]}#{' ' * (matrix[1].max_size - matrix[1][row].to_s.size)} " +
-    "#{matrix[2][row]}#{' ' * (matrix[2].max_size - matrix[2][row].to_s.size)}"
+       "#{matrix[1][row]}#{' ' * (matrix[1].max_size - matrix[1][row].to_s.size)} " +
+       "#{matrix[2][row]}#{' ' * (matrix[2].max_size - matrix[2][row].to_s.size)}"
 end
